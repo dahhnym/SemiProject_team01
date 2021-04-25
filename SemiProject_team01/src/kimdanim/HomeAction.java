da378decaf92dcb4db9da807c22c33bdf98aa1e2
Danim Kim <kdn725@gmail.com>
2021-04-26 00:41:05 +0900

    HomeAction 만듦

---
+++ SemiProject_team01/src/kimdanim/HomeAction.java
@@ -0,0 +1,18 @@
+package kimdanim;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class HomeAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		// super.setRedirect(false);
+		super.setViewPage("/WEB-INF/kimdanim/home.jsp");
+		
+	}
+
+}
