d25d6269578c4e970420342e82738f744ff53e68
yehyoni522 <78483533+yehyoni522@users.noreply.github.com>
2021-04-27 20:22:47 +0900

    21/04/27 20:22 고객센터_자주묻는질문_첫업로드

---
+++ SemiProject_team01/src/cscenter/controller/CsHomeAction.java
@@ -0,0 +1,17 @@
+package cscenter.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class CsHomeAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		super.setViewPage("/WEB-INF/cscenter/CsHome.jsp");
+		
+	}
+
+}
