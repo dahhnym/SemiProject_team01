dd68c0f4783b9fe0667a4c9323fbec0bff3e23ef
yehyoni522 <78483533+yehyoni522@users.noreply.github.com>
2021-04-28 22:49:18 +0900

    21/04/28 22:49

--- SemiProject_team01/src/cscenter/controller/CsBoardAction.java
+++ SemiProject_team01/src/cscenter/controller/CsBoardAction.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
 
-public class CsHomeAction extends AbstractController {
+public class CsBoardAction extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		//super.setRedirect(true);
-		super.setViewPage("/WEB-INF/cscenter/CsHome.jsp");
+		super.setViewPage("/WEB-INF/cscenter/board_All.jsp");
 		
 	}
 
