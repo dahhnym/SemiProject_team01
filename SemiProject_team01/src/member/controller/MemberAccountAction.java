4504b6f29b308d6f68ba48e464232cf484eed3c0
OhDayoon <dayoon1013@gmail.com>
2021-04-30 14:08:04 +0900

    2021 04 30

--- SemiProject_team01/src/member/controller/MemberAccountAction.java
+++ SemiProject_team01/src/member/controller/MemberAccountAction.java
@@ -1,18 +1,18 @@
-package cart.controller;
-
+package member.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
 
-public class CartAction extends AbstractController {
+public class MemberAccountAction extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		super.setRedirect(false);
-		super.setViewPage("/WEB-INF/cart/cart.jsp");
+		super.setViewPage("/WEB-INF/member/memberaccount.jsp");
+		
 	}
 
 }
