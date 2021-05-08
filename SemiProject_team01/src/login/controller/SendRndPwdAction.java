f0448d173c52d746f92f27606e374448fafe9063
Ilpyo Yang <67682215+Ilpyo-Yang@users.noreply.github.com>
2021-05-09 05:24:24 +0900

    5/9 오전5:23  [비밀번호 찾기 페이지] 메일문구 수정 외

--- SemiProject_team01/src/login/controller/SendRndPwdAction.java
+++ SemiProject_team01/src/login/controller/SendRndPwdAction.java
@@ -15,20 +15,25 @@ public class SendRndPwdAction extends AbstractController {
 			String email = request.getParameter("email");
 			
 			///////////////////////////////////////////////////////////////
-			// 랜덤 인증번호(영문자, 숫자 10자리) 생성
+			// 랜덤 인증번호(영문자, 숫자, 특수기호 10자리) 생성
 			Random rnd = new Random();
 			String rndPwd = "";	
 			
 			char randchar = ' ';
-			for (int i=0; i<5; i++) {
+			for (int i=0; i<4; i++) {
 				randchar = (char)(rnd.nextInt('z' - 'a' + 1) + 'a');
 				rndPwd += randchar;				
 			}			
 			int randnum = 0;
-			for (int i=0; i<5; i++) {
+			for (int i=0; i<4; i++) {
 				randnum = rnd.nextInt(9 - 0 + 1) + 0;
 				rndPwd += randnum;
-			}			
+			}
+			randchar = ' ';
+			for (int i=0; i<4; i++) {
+				randchar = (char)(rnd.nextInt('+' - '!' + 1) + '!');
+				rndPwd += randchar;				
+			}
 			//////////////////////////////////////////////////////////////
 			
 			boolean sendMailSuccess = false; // 메일이 정상적으로 전송되었는지 유무를 알아오기 위한 용도	
